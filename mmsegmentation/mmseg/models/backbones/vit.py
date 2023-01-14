training with fixed loss functions and normalization

mmsegmentation/mmseg/models/backbones/vit.py
@@ -199,7 +199,6 @@ class VisionTransformer(BaseModule):
                  pretrained=None,
                  init_cfg=None):
         super(VisionTransformer, self).__init__(init_cfg=init_cfg)
-
         if isinstance(img_size, int):
             img_size = to_2tuple(img_size)
         elif isinstance(img_size, tuple):
@@ -398,6 +397,9 @@ class VisionTransformer(BaseModule):
         return pos_embed
 
     def forward(self, inputs):
+        save_dir = "/home/emir/Desktop/dev/model_outputs/"
+        print("i am insid vit")
+        mpimg.imsave(save_dir+"input_of_vision_transformer.png", inputs)
         B = inputs.shape[0]
 
         x, hw_shape = self.patch_embed(inputs)
@@ -429,7 +431,7 @@ class VisionTransformer(BaseModule):
                 if self.output_cls_token:
                     out = [out, x[:, 0]]
                 outs.append(out)
-
+        mpimg.imsave(save_dir+"output_of_vit.png", outs)
         return tuple(outs)
 
     def train(self, mode=True):
@@ -438,3 +440,4 @@ class VisionTransformer(BaseModule):
             for m in self.modules():
                 if isinstance(m, nn.LayerNorm):
                     m.eval()
+import matplotlib.image as mpimg

mmsegmentation/mmseg/models/decode_heads/atm_head.py
@@ -7,12 +7,11 @@ from typing import Optional
 import math
 from functools import partial
 from mmcv.runner import auto_fp16, force_fp32
-import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 from mmseg.models.builder import HEADS
 from mmseg.models.decode_heads.decode_head import BaseDecodeHead
 from timm.models.layers import trunc_normal_
-import matplotlib.pyplot as plt
 from mmseg.models.losses import accuracy
 
 def trunc_normal_init(module: nn.Module,
@@ -196,6 +195,9 @@ class ATMHead(BaseDecodeHead):
                 constant_init(m, val=1.0, bias=0.0)
 
     def forward(self, inputs):
+        # save_dir = "/home/emir/Desktop/dev/model_outputs/"
+        # mpimg.imsave(save_dir+"input_of_ATM_head.png", inputs)
+        # print("saving image in atm head")
         x = []
         for stage_ in inputs[:self.use_stages]:
             x.append(self.d4_to_d3(stage_) if stage_.dim() > 3 else stage_)